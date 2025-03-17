6e5c09b0023f8c8f94a7fb7728ff1ee24e86d876
gregwilliam <122914452+gregwilliam@users.noreply.github.com>
2025-03-17 12:24:25 +0000

    Add files via upload

---
+++ Challenge 3/Team 3A/submission/power_bi_chatbot/chatbot_project/chatbot/staticfiles/chatbot/app.js
@@ -0,0 +1,11 @@
+var message_timeout = document.getElementById('message-timer');
+if (message_timeout){
+    setTimeout(function(){
+    message_timeout.style.display= 'none';
+    },2000);
+}
+
+
+
+
+
